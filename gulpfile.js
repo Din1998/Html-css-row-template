ffc49a717ba97880ce614bdb6860ce35555928ff
din1998 <dinislam8803@gmail.com>
2023-02-22 23:40:23 +0600

    add feature section

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const {src,dest,watch,series} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyle() {
-  return src('*.scss')
+  return src('projectUiLib/**/*.scss')
   .pipe(sass())
   .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['*.scss'],buildStyle)
+  watch(['projectUiLib/**/*.scss'],buildStyle)
 }
 
 exports.default = series(buildStyle,watchTask);
